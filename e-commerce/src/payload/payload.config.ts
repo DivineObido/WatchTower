6ac95c0c9b09e5c22bd37188bb66ff721025e760
Divine Obido <divineobido64@gmail.com>
2025-12-18 01:58:07 +0100

    fixed prettier error

--- e-commerce/src/payload/payload.config.ts
+++ e-commerce/src/payload/payload.config.ts
@@ -42,7 +42,7 @@ dotenv.config({
 export default buildConfig({
   admin: {
     user: Users.slug,
-    disable : false,
+    disable: false,
     bundler: webpackBundler(), // bundler-config
     components: {
       // The `BeforeLogin` component renders a message that you see while logging into your admin panel.
